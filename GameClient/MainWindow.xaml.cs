7310b2205b52c8ebc3ad8297ddb9047abe559344
dmitesh6730 <dmitesh6730@gmail.com>
2019-04-02 00:37:30 -0400

    test 1 for connection ME

--- GameClient/MainWindow.xaml.cs
+++ GameClient/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.ServiceModel;
+using GameLibrary;
 
 namespace GameClient
 {
@@ -20,11 +22,46 @@ namespace GameClient
     /// </summary>
     public partial class MainWindow : Window
     {
+        IMe me = null;
         public MainWindow()
         {
             InitializeComponent();
-            this.ShowDialog();
+
+            try
+            {
+                // Connect to the WCF service endpoint called "ShoeService" 
+                //ChannelFactory<IShoe> channel = new ChannelFactory<IShoe>(new NetTcpBinding(), 
+                //    new EndpointAddress("net.tcp://localhost:13200/CardsLibrary/ShoeService"));
+                ChannelFactory<IMe> channel = new ChannelFactory<IMe>("ShoeEndpoint");
+                me = channel.CreateChannel();
+
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+
+
+        
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            me=new Me();
             
+            try
+            {
+                me.Name_ = tBox.Text;
+                l.Items.Insert(0,me.Name_);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
