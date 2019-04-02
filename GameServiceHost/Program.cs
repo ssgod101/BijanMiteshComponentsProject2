6d84624c819516587274ac39d1e1a885620c8eb9
dmitesh6730 <dmitesh6730@gmail.com>
2019-04-01 23:01:16 -0400

    servicehost ready

--- GameServiceHost/Program.cs
+++ GameServiceHost/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+using GameLibrary;
 
 namespace GameServiceHost
 {
@@ -10,6 +12,31 @@ namespace GameServiceHost
     {
         static void Main(string[] args)
         {
+            ServiceHost servHost = null;
+            try
+            {
+                // Register the service Address
+                //servHost = new ServiceHost(typeof(Shoe), new Uri("net.tcp://localhost:13200/CardsLibrary/"));
+                servHost = new ServiceHost(typeof(Me));
+
+                // Register the service Contract and Binding
+                // servHost.AddServiceEndpoint(typeof(IShoe), new NetTcpBinding(), "ShoeService");
+
+                // Run the service
+                servHost.Open();
+                Console.WriteLine("Service started. Press any key to quit.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                // Wait for a keystroke
+                Console.ReadKey();
+                if (servHost != null)
+                    servHost.Close();
+            }
         }
     }
 }
