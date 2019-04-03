08735eac4335aa029b8fb34b5126936d268f69ac
Bijan Khajavi <mona3000@hotmail.ca>
2019-04-03 01:15:16 -0400

    Added extra funcionality to deck with more generic features.

--- Crazy8Library/CallbackInfo.cs
+++ Crazy8Library/CallbackInfo.cs
@@ -10,17 +10,22 @@ namespace Crazy8Library
     [DataContract]
     public class CallbackInfo
     {
-        //Lobby Info
+        //Game Info
         [DataMember] public int topSuit { get; private set; }
         [DataMember] public int topRank { get; private set; }
-        //Game Info
+        [DataMember] public bool emptyHand { get; private set; }
+        [DataMember] public bool drawHand { get; private set; }
+
+        //Lobby Info
         [DataMember] public int numPlayers { get; private set; }
         [DataMember] public string[] playerNames { get; private set; }
 
-        public CallbackInfo(int topS, int topR, int numPlayers)
+        public CallbackInfo(int topS, int topR, bool empty, bool draw)
         {
             topSuit = topS;
             topRank = topR;
+            emptyHand = empty;
+            drawHand = draw;
         }
 
         public CallbackInfo(int num, string[] names)
