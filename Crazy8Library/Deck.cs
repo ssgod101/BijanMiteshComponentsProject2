9baac5011e9dfcdb5994048ceef3a8a46b7b43b8
dmitesh6730 <dmitesh6730@gmail.com>
2019-04-05 02:42:55 -0400

    rtyjtuy

--- Crazy8Library/Deck.cs
+++ Crazy8Library/Deck.cs
@@ -239,8 +239,9 @@ namespace Crazy8Library
                 }
                 if(placed.Rank == Card.RankID.Two){TwoChain+=2;}
                 else{TwoChain = 0;}
-                EndTurn(name);
+             
             }
+            EndTurn(name);
             return true;
         }
 
